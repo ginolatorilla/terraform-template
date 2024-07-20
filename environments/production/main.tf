b4c99392e15e326691ed4f373cc500801c72794a
Gino Latorilla <pblatorilla+github@gmail.com>
2024-07-21 00:41:48 +0800

    Remove main.tf from the base

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,1 +1,10 @@
-../../base/main.tf
+resource "local_file" "this" {
+  content  = "Hello"
+  filename = "hello-production.txt"
+}
+
+module "randomfile" {
+  source = "../../modules/randomfile"
+  length = 16
+  path   = "random-${var.path}"
+}
--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,1 +1,10 @@
-../../base/main.tf
+resource "local_file" "this" {
+  content  = "Hello"
+  filename = "hello-staging.txt"
+}
+
+module "randomfile" {
+  source = "../../modules/randomfile"
+  length = 16
+  path   = "random-${var.path}"
+}
